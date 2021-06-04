9f3535c63e307ed03e00f518b5b0190c6503593e
Brian Ding <brian_ding@outlook.com>
2021-06-04 15:37:41 +0800

    feat: btree delete

--- btree.go
+++ btree.go
@@ -1,6 +1,8 @@
 package sogstruct
 
-import "math"
+import (
+	"math"
+)
 
 type btreenode struct {
 	m        int
@@ -139,4 +141,159 @@ func (node *btreenode) addkey(value int) bool {
 	node.keys = temp
 
 	return len(node.keys) <= node.m-1
+}
+
+func (tree *btree) Delete(value int) {
+	// find where is the value in the tree
+	node := tree.root.search(value)
+	if node == nil {
+		// value not in the tree
+		return
+	}
+
+	node.delete(value, tree)
+}
+
+func (node *btreenode) delete(value int, tree *btree) {
+	if len(node.children) == 0 {
+		// leaf node
+		// remove the key directory
+		temp := node.keys
+		node.keys = make([]int, 0, len(temp)-1)
+		for _, v := range temp {
+			if v != value {
+				node.keys = append(node.keys, v)
+			}
+		}
+	} else {
+		// internal node
+		var index int
+		for i, v := range node.keys {
+			if v == value {
+				index = i
+				break
+			}
+		}
+
+		// child before the key has more than minimum keys
+		// remove the key, and fill in child's last key
+		if len(node.children[index].keys) > int(math.Ceil(float64(node.m)/2))-1 {
+			node.keys[index] = node.children[index].keys[len(node.children[index].keys)-1]
+			node.children[index].keys = node.children[index].keys[:len(node.children[index].keys)-1]
+		} else if len(node.children[index+1].keys) > int(math.Ceil(float64(node.m)/2))-1 {
+			// child after the key has more than minimum keys
+			// remove the key, and fill in child's first key
+			node.keys[index] = node.children[index+1].keys[0]
+			node.children[index+1].keys = node.children[index+1].keys[1:]
+		} else {
+			tempKeys := node.keys
+			node.keys = make([]int, 0, len(tempKeys)-1)
+			for _, v := range tempKeys {
+				if v != value {
+					node.keys = append(node.keys, v)
+				}
+			}
+
+			tempChildren := node.children
+			node.children = make([]*btreenode, 0, len(tempChildren)-1)
+			for i := 0; i < len(tempChildren); i++ {
+				child := &btreenode{m: node.children[i].m, keys: node.children[i].keys, children: node.children[i].children, parent: node.children[i].parent}
+				node.children = append(node.children, child)
+				if i == index {
+					i++
+					child = &btreenode{m: node.children[i].m, keys: node.children[i].keys, children: node.children[i].children, parent: node.children[i].parent}
+					node.children = append(node.children, child)
+				}
+			}
+		}
+	}
+
+	if len(node.keys) < int(math.Ceil(float64(node.m)/2))-1 {
+		// number of keys less than allowed minimum
+		// get one key from its parent
+		node.stealpkey(tree)
+	}
+}
+
+func (node *btreenode) stealpkey(tree *btree) {
+	// root
+	if node.parent == nil {
+		if len(node.keys) == 0 {
+			tree.root.keys = node.children[0].keys
+			tree.root.keys = append(tree.root.keys, node.children[1].keys...)
+
+			tree.root.children = node.children[0].children
+			tree.root.children = append(tree.root.children, node.children[1].children...)
+		} else {
+			merged := false
+			for i, v := range node.keys {
+				if merged {
+					node.children[i].keys = node.children[i+1].keys
+					node.children[i].children = node.children[i+1].children
+				} else if node.children[i+1].keys[0] < v {
+					node.children[i].keys = append(node.children[i].keys, node.children[i+1].keys...)
+					node.children[i].children = append(node.children[i].children, node.children[i+1].children...)
+
+					merged = true
+				}
+			}
+
+			if !merged {
+				node.children[len(node.children)-2].keys = append(node.children[len(node.children)-2].keys, node.children[len(node.children)-1].keys...)
+				node.children[len(node.children)-2].children = append(node.children[len(node.children)-2].children, node.children[len(node.children)-1].children...)
+			}
+
+			node.children = node.children[:len(node.keys)+1]
+		}
+		return
+	}
+
+	var index int
+	for i, v := range node.parent.children {
+		if v == node {
+			index = i
+			break
+		}
+	}
+
+	// the key stolen from parent
+	var key int
+	if index == len(node.parent.keys) {
+		// if the last children, get the last key from parent
+		index--
+		temp := node.parent.keys
+		key = temp[index]
+		node.parent.keys = make([]int, 0, len(temp)-1)
+		for i, v := range temp {
+			if i != index {
+				node.parent.keys = append(node.parent.keys, v)
+			}
+		}
+	}
+	node.addkey(key)
+	node.parent.stealpkey(tree)
+}
+
+func (node *btreenode) search(value int) *btreenode {
+	if len(node.children) == 0 {
+		// no children, leaf node
+		for _, v := range node.keys {
+			if v == value {
+				return node
+			}
+		}
+
+		return nil
+	}
+
+	// not leaf node
+	for i, v := range node.keys {
+		if value < v {
+			return node.children[i].search(value)
+		} else if value == v {
+			return node
+		}
+	}
+
+	return node.children[len(node.keys)].search(value)
 }
--- btree_test.go
+++ btree_test.go
@@ -52,3 +52,44 @@ func TestBTree(t *testing.T) {
 		t.Errorf("key of btree's children[3] is not [80]")
 	}
 }
+
+func TestBTree2(t *testing.T) {
+	tree := newBTree(4)
+	tree.Insert(10)
+	tree.Insert(20)
+	tree.Insert(40)
+	tree.Insert(50)
+	tree.Insert(60)
+	tree.Insert(70)
+	tree.Insert(80)
+	tree.Insert(30)
+	tree.Insert(35)
+
+	tree.Delete(10)
+	tree.Delete(40)
+	tree.Delete(80)
+
+	if want, got := 30, tree.root.keys[0]; got != want {
+		t.Errorf("key of btree's root is not [30]")
+	}
+
+	if want, got := 50, tree.root.keys[1]; got != want {
+		t.Errorf("key of btree's root is not [50]")
+	}
+
+	if want, got := 20, tree.root.children[0].keys[0]; got != want {
+		t.Errorf("key of btree's children[0] is not [20]")
+	}
+
+	if want, got := 35, tree.root.children[1].keys[0]; got != want {
+		t.Errorf("key of btree's children[1] is not [35]")
+	}
+
+	if want, got := 60, tree.root.children[2].keys[0]; got != want {
+		t.Errorf("key of btree's children[2] is not [60]")
+	}
+
+	if want, got := 70, tree.root.children[2].keys[1]; got != want {
+		t.Errorf("key of btree's children[2] is not [70]")
+	}
+}
